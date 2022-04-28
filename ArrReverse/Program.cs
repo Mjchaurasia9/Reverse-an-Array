cf104274dfa3dcf7efca598f0be6b72dbfd20fb6
Mjchaurasia9 <92732297+Mjchaurasia9@users.noreply.github.com>
2022-04-28 17:36:00 +0530

    Add files via upload

---
+++ ArrReverse/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("How many Element You Want to Add in Array:\t");
+int num = int.Parse(Console.ReadLine());
+
+int [] arr = new int [num];
+
+for (int i = 0; i < arr.Length; i++)
+{
+Console.Write("Enter the Value Of Element:\t");
+int ele = int.Parse(Console.ReadLine());
+arr[i] = ele;
+}
+
+Array.Reverse(arr);
+Console.WriteLine("Reverse Element of Given Array:\t"+ String.Join(',',arr));
+
+/*for (int j = arr.Length-1; j >= 0; j--)
+{
+Console.Write(arr[j]+" ");
+}
+Console.WriteLine();
+*/
